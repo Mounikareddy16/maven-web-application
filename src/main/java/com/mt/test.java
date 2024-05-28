1a688557d0a7b26c4afb5b88fe79aeaaf9310093
Mounikareddy16 <45049658+Mounikareddy16@users.noreply.github.com>
2024-05-29 00:37:15 +0530

    Update test.java

--- src/main/java/com/mt/test.java
+++ src/main/java/com/mt/test.java
@@ -1,9 +1,1 @@
-public class XSS extends HttpServlet {
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-	throws ServletException, IOException {
-		// BAD: a request parameter is written directly to the Servlet response stream
-		response.getWriter().print(
-				"The page \"" + request.getParameter("page") + "\" was not found.");
 
-	}
-}
